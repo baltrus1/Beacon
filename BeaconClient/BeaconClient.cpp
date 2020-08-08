c1714df24f9b8a78bf340e6e4956e3109476d405
Baltrus <b.sivickis@gmail.com>
2020-08-08 12:19:57 +0300

    initial commit

---
+++ BeaconClient/BeaconClient.cpp
@@ -0,0 +1,13 @@
+﻿// BeaconClient.cpp : Defines the entry point for the application.
+//
+
+#include "BeaconClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello from client!" << endl;
+	std::cin.get();
+	return 0;
+}
---
+++ BeaconServer/BeaconServer.cpp
@@ -0,0 +1,13 @@
+﻿// BeaconServer.cpp : Defines the entry point for the application.
+//
+
+#include "BeaconServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello from server." << endl;
+	std::cin.get();
+	return 0;
+}
