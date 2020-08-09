557c2af64eddbf024fa980cde730d8f943551ff1
Baltrus <b.sivickis@gmail.com>
2020-08-10 02:22:04 +0300

    added server communication with multiple clients, sending server list to them, clients now format received data and sort servers.

--- BeaconClient/BeaconClient.cpp
+++ BeaconClient/BeaconClient.cpp
@@ -1,46 +1,123 @@
 ﻿// BeaconClient.cpp : Defines the entry point for the application.
 //
 
-#include "BeaconClient.h"
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
 
 #include "boost/asio.hpp"
 #include "boost/array.hpp"
+#include <boost/asio/ssl.hpp>
+
+#include <json/json.h>
+#include <json/reader.h>
 
 #define BROADCAST_PORT 1234
+#define P2P_PORT 4321
+#define MAX_LENGTH 3000
 
-int main()
+namespace ba = boost::asio;
+static Json::Reader reader;
+
+std::string getData(boost::asio::io_context& io_context, const ba::ip::udp::endpoint& broadcastSource)
 {
-	namespace ba = boost::asio;
+    ba::ip::udp::socket socket(io_context);
+    boost::system::error_code ec;
+    socket.open(ba::ip::udp::v4());
 
-	ba::io_service io_service;
+    std::string request = "Give server list pls";
+    size_t request_length = request.length();
 
-    try
-    {
-        while(true)
-        {
-            auto socket = ba::ip::udp::socket(io_service, 
+    ba::ip::udp::endpoint destination = ba::ip::udp::endpoint(broadcastSource.address(), P2P_PORT);
+    socket.send_to(boost::asio::buffer(request, request_length), destination, 0, ec);
+
+    if (ec) {
+        std::cout << "CLIENT: Send failed: " << ec.message() << "\n";
+    }
+
+    std::cout << "CLIENT: Request sent! " << std::endl;
+
+    boost::array<char, MAX_LENGTH> recv_buf;
+    ba::ip::udp::endpoint sender_endpoint;
+    size_t len = socket.receive_from(
+        boost::asio::buffer(recv_buf), sender_endpoint);
+
+    std::string data(recv_buf.c_array(), len);
+    std::cout << "CLIENT: Received data length: " << len << std::endl;
+    return data;
+}
+
+void displayData(const std::string data) {
+    Json::Value root;
+    std::string doc = data;
+
+    const char* begin = doc.c_str();
+    const char* end = begin + doc.length();
+
+    bool parsingSuccessful = reader.parse(begin, end, root);
+    if (!parsingSuccessful) {
+        std::cout << "CLIENT: Json parsing failed" << std::endl;
+    }
+
+    std::vector<std::pair<int, const char*>> servers;
+    const char* buffer;
+
+    assert(root.isArray());
+    for (int i = 0; i < root.size(); ++i) {
+        int d = root[i]["distance"].asInt();
+        buffer = root[i]["name"].asCString();
+        servers.emplace_back(d, buffer);
+    }
+
+    std::sort(servers.begin(), servers.end(), [](const auto& left, const auto& right) {
+        return left.first < right.first;
+        });
+    
+    std::cout << "\nCLIENT: List of servers sorted by their distance:\n";
+    for (const auto& pair : servers) {
+        std::cout << "Server: " << pair.second;
+        std::cout << ", distance: " << pair.first << std::endl;
+    }
+}
+
+int main() {
+	ba::io_context io_context;
+
+    try {
+        // In case we get unexpected packet we want to setup again
+        while (true) {
+            auto socket = ba::ip::udp::socket(io_context,
                 ba::ip::udp::endpoint(ba::ip::udp::v4(), BROADCAST_PORT));
             boost::array<char, 100> recv_buf;
             ba::ip::udp::endpoint remote_endpoint;
-            boost::system::error_code error;
+            boost::system::error_code ec;
             socket.receive_from(boost::asio::buffer(recv_buf),
-                remote_endpoint, 0, error);
+                remote_endpoint, 0, ec);
 
-            if (error && error != boost::asio::error::message_size)
-                throw boost::system::system_error(error);
+            if (ec && ec != boost::asio::error::message_size) {
+                std::cout << "CLIENT: Error occured while receiving data: " << ec.message() << std::endl;
+            }
 
-            std::cout << "Data received: " << std::endl;
-            std::cout << recv_buf.c_array() << std::endl;
-            /*std::string message = "hello from client";
 
-            boost::system::error_code ignored_error;
-            socket.send_to(boost::asio::buffer(message),
-                remote_endpoint, 0, ignored_error);*/
+            if (std::string(recv_buf.c_array()) != "I am still here") {
+                continue;
+            }
+
+            std::cout << "CLIENT: Connecting to server..." << std::endl;
+
+            std::string data = getData(io_context, remote_endpoint);
+
+            displayData(data);
+
+            io_context.run();
+            std::this_thread::sleep_for(std::chrono::seconds(200));
+            break;
         }
-    } catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
+    } catch ( ... ) {
+        std::exception_ptr eptr = std::current_exception();
     }
+
+    io_context.run();
 
 	return 0;
 
--- BeaconServer/BeaconServer.cpp
+++ BeaconServer/BeaconServer.cpp
@@ -1,242 +1,255 @@
 ﻿// BeaconServer.cpp : Defines the entry point for the application.
 //
 
-#include "BeaconServer.h"
+#include <iostream>
 
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <boost/bind/bind.hpp>
+
+#include <queue>
 #include <memory>
 
 #define BROADCAST_PORT 1234
-#define MAX_LENGTH 1024
+#define P2P_PORT 4321
+#define MAX_LENGTH 3024
+
 
 namespace ba = boost::asio;
 
+static boost::asio::io_context io_context;
+
 class PeriodicBroadcasting {
-    int m_interval = 1;
-    std::atomic<bool> m_running = false;
-    std::unique_ptr<std::thread> m_thread;
-    boost::system::error_code m_error;
+	int m_interval = 1;
+	std::atomic<bool> m_running = false;
+	std::unique_ptr<std::thread> m_thread;
+	boost::system::error_code m_error;
 public:
-    PeriodicBroadcasting(int interval) 
-    : m_interval(interval)
-    {
-    }
-    
-    ~PeriodicBroadcasting() {
-        stop();
-    }
+	PeriodicBroadcasting(int interval)
+		: m_interval(interval)
+	{
+	}
 
-    void start(const std::shared_ptr<ba::ip::udp::socket>& socket, ba::const_buffer buffer, const ba::ip::udp::endpoint& remote_endpoint) {
-        m_running = true;
-        m_thread = std::make_unique<std::thread>(&PeriodicBroadcasting::periodicallySend, this, socket, buffer, remote_endpoint);
-    }
+	~PeriodicBroadcasting() {
+		stop();
+	}
 
-    void stop() {
-        if (m_running) {
-            m_running = false;
-            m_thread->join();
-        }
-    }
+	void start(const std::shared_ptr<ba::ip::udp::socket>& socket, ba::const_buffer buffer, const ba::ip::udp::endpoint& remote_endpoint) {
+		m_running = true;
+		m_thread = std::make_unique<std::thread>(&PeriodicBroadcasting::periodicallySend, this, socket, buffer, remote_endpoint);
+	}
 
-    void periodicallySend(const std::shared_ptr<ba::ip::udp::socket>& socket, ba::const_buffer& buffer, const ba::ip::udp::endpoint& remote_endpoint) {
-        while (m_running) {
-            try {
-                socket->send_to(buffer, remote_endpoint, 0, m_error);
-                if (m_error) {
-                    std::cerr << "Send failed: " << m_error.message() << std::endl;
-                }
-            }
-            catch (...) {}
-            std::this_thread::sleep_for(std::chrono::seconds(m_interval));
-        }
-    }
+	void stop() {
+		if (m_running) {
+			m_running = false;
+			m_thread->join();
+		}
+	}
+
+	void periodicallySend(const std::shared_ptr<ba::ip::udp::socket>& socket, ba::const_buffer& buffer, const ba::ip::udp::endpoint& remote_endpoint) {
+		while (m_running) {
+			try {
+				socket->send_to(buffer, remote_endpoint, 0, m_error);
+				if (m_error) {
+					std::cerr << "Broadcast send failed: " << m_error.message() << std::endl;
+				}
+				std::this_thread::sleep_for(std::chrono::seconds(m_interval));
+			}
+			catch (...) {}
+		}
+	}
 
 };
+
 
 // modified version of https://www.boost.org/doc/libs/1_73_0/doc/html/boost_asio/example/cpp11/ssl/client.cpp
-class client
-{
+class Proxy {
 public:
-    client(boost::asio::io_context& io_context,
-        boost::asio::ssl::context& context,
-        const ba::ip::tcp::resolver::results_type& endpoints)
-        : socket_(io_context, context)
-    {
-        socket_.set_verify_mode(boost::asio::ssl::verify_peer);
-        socket_.set_verify_callback(
-            std::bind(&client::verify_certificate, this, std::placeholders::_1, std::placeholders::_2));
+	explicit Proxy(ba::ip::udp::endpoint& clientEndpoint)
+		: ctx(boost::asio::ssl::context::sslv23_client)
+		, socket_(io_context, ctx)
+		, m_clientEndpoint(clientEndpoint)
+	{
+		ba::ip::tcp::resolver resolver(io_context);
+		endpoints_ = resolver.resolve("playground.tesonet.lt", "https");
+		socket_.set_verify_mode(boost::asio::ssl::verify_none);
 
-        connect(endpoints);
-    }
+		// From here everything is done async
+		connect();
+	}
+
+	void connect() {
+		ba::async_connect(socket_.lowest_layer(), endpoints_,
+			[this](const boost::system::error_code& error,
+				const ba::ip::tcp::endpoint& /*endpoint*/) {
+				if (!error) {
+					handshake();
+				} else {
+					std::cout << "Connect failed: " << error.message() << "\n";
+				}
+			});
+	}
 
 private:
-    bool verify_certificate(bool preverified,
-        boost::asio::ssl::verify_context& ctx)
-    {
-        // The verify callback can be used to check whether the certificate that is
-        // being presented is valid for the peer. For example, RFC 2818 describes
-        // the steps involved in doing this for HTTPS. Consult the OpenSSL
-        // documentation for more details. Note that the callback is called once
-        // for each certificate in the certificate chain, starting from the root
-        // certificate authority.
+	void handshake() {
+		socket_.async_handshake(boost::asio::ssl::stream_base::client,
+			[this](const boost::system::error_code& error) {
+				if (!error) {
+					send_request();
+				} else {
+					std::cout << "Handshake failed: " << error.message() << "\n";
+				}
+			});
+	}
 
-        // In this example we will simply print the certificate's subject name.
-        /*char subject_name[256];
-        X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
-        X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
-        std::cout << "Verifying " << subject_name << "\n";
+	void send_request()
+	{
+		std::ostringstream request_stream;
+		//std::string json = "{\"username\":\"tesonet\",\"password\":\"partyanimal\"}";
 
-        return preverified;*/
+		request_stream << "GET /v1/servers HTTP/1.1 \r\n";
+		request_stream << "Host: " << "playground.tesonet.lt" << "\r\n";
+		request_stream << "User-Agent: curl/7.55.1" << "\r\n";
+		request_stream << "Accept: */*\r\n";
+		request_stream << "Connection: close\r\n";
+		request_stream << "Authorization: Bearer f9731b590611a5a9377fbd02f247fcdf\r\n\r\n";
 
-        return true;
-    }
 
-    void connect(const ba::ip::tcp::resolver::results_type& endpoints)
-    {
-        ba::async_connect(socket_.lowest_layer(), endpoints,
-            [this](const boost::system::error_code& error,
-                const ba::ip::tcp::endpoint& /*endpoint*/)
-            {
-                if (!error)
-                {
-                    handshake();
-                }
-                else
-                {
-                    std::cout << "Connect failed: " << error.message() << "\n";
-                }
-            });
-    }
+		/*request_stream << "Content-Type: application/json\r\n";
+		request_stream << "Content - Length: " << json.length() << "\r\n\r\n";
+		request_stream << json << "\r\n";*/
 
-    void handshake()
-    {
-        socket_.async_handshake(boost::asio::ssl::stream_base::client,
-            [this](const boost::system::error_code& error)
-            {
-                if (!error)
-                {
-                    send_request();
-                }
-                else
-                {
-                    std::cout << "Handshake failed: " << error.message() << "\n";
-                }
-            });
-    }
 
-    void send_request()
-    {
-        std::ostringstream request_stream;
-        //std::string json = "{'username' : 'tesonet',  'password' : 'partyanimal' }";
-        std::string json = "{\"username\" : \"tesonet\",  \"password\" : \"partyanimal\" }";
-        //std::string json = "username=tesonet&password=partyanimal";
+		auto request_str = request_stream.str();
+		//std::cout << "Request: " << std::endl << std::endl << request_str << std::endl;
 
-        request_stream << "POST /v1/tokens HTTP/1.1 \r\n";
-        request_stream << "Host: " << "playground.tesonet.lt" << "\r\n";
-        request_stream << "User-Agent: curl/7.55.1" << "\r\n";
-        request_stream << "Accept: */*\r\n";
-        request_stream << "Content-Type: application/json\r\n";
-        request_stream << "Content - Length: " << json.length() << "\r\n\r\n";
-        request_stream << json << "\r\n";
+		ba::async_write(socket_,
+			boost::asio::buffer(request_str, request_str.length()),
+			[this](const boost::system::error_code& error, std::size_t length) {
+				if (!error) {
+					receive_response();
+				} else {
+					std::cout << "Write failed: " << error.message() << "\n";
+				}
+			});
+	}
 
-        auto request_str = request_stream.str();
-        std::cout << "Request: " << std::endl << std::endl << request_str << std::endl << std::endl;
+	void receive_response()
+	{
+		boost::asio::async_read(socket_,
+			boost::asio::buffer(reply_),
+			[this](const boost::system::error_code& error, std::size_t length) {
+				// Error will occur as soon as server has no more data and closes connection and then this will execute
+				resendToClient(length);
+			});
+	}
 
-        ba::async_write(socket_,
-            boost::asio::buffer(request_str, request_str.length()),
-            [this](const boost::system::error_code& error, std::size_t length)
-            {
-                if (!error)
-                {
-                    receive_response(length);
-                }
-                else
-                {
-                    std::cout << "Write failed: " << error.message() << "\n";
-                }
-            });
-    }
+	void resendToClient(int length) {
+		std::string response(reply_, length);
+		size_t startPos = response.find("[{\"");
+		size_t endPos = response.find("}]");
+		std::string body = response.substr(startPos, endPos - startPos + 2);
+		std::cout << "SERVER: Sending data to client:\n" << body << std::endl << std::endl;
 
-    void receive_response(std::size_t length)
-    {
-        boost::asio::async_read(socket_,
-            boost::asio::buffer(reply_, length),
-            [this](const boost::system::error_code& error, std::size_t length)
-            {
-                if (!error)
-                {
-                    std::cout << "Reply: ";
-                    std::cout.write(reply_, length);
-                    std::cout << "\n";
-                }
-                else
-                {
-                    std::cout << "Read failed: " << error.message() << "\n";
-                }
-            });
-    }
+		boost::system::error_code ec;
+		ba::ip::udp::socket sock(io_context);
 
-    boost::asio::ssl::stream<ba::ip::tcp::socket> socket_;
-    char request_[MAX_LENGTH];
-    char reply_[MAX_LENGTH];
+		sock.open(ba::ip::udp::v4(), ec);
+		if (ec) {
+			std::cerr << "SERVER: Socket open failed: " << ec.message() << std::endl;
+			return;
+		}
+
+		sock.send_to(boost::asio::buffer(body, body.length()), m_clientEndpoint, 0, ec);
+		if (ec) {
+			std::cout << "SERVER: Failed to send server list to client: " << ec.message() << std::endl;
+			return;
+		}
+	}
+
+	ba::ssl::context ctx;
+	ba::ip::tcp::resolver::results_type endpoints_;
+	ba::ssl::stream<ba::ip::tcp::socket> socket_;
+	ba::ip::udp::endpoint m_clientEndpoint;
+	char request_[MAX_LENGTH];
+	char reply_[MAX_LENGTH];
 };
 
-int main(int argc, char* argv[])
+// modified version of https://www.boost.org/doc/libs/1_73_0/doc/html/boost_asio/tutorial/tutdaytime6/src.html
+// This udp_server is responsible for catching client requests, creating Proxy object for each 
+// such client and returning data from Proxy object to that client.
+class udp_server
 {
-    ba::io_context io_context;
+public:
+	udp_server()
+		: socket_(io_context, ba::ip::udp::endpoint(ba::ip::udp::v4(), P2P_PORT))
+	{
+		start_receive();
+	}
 
-    /*ba::ip::udp::resolver resolver(io_service);
-    ba::ip::udp::resolver::query query(ba::ip::host_name(), "");
-    ba::ip::udp::resolver::iterator it = resolver.resolve(query);
+private:
+	void start_receive()
+	{
+		socket_.async_receive_from(
+			boost::asio::buffer(recv_buffer_), source_endpoint,
+			boost::bind(&udp_server::handle_receive, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+	}
 
-    ba::ip::address local_addr;
+	void handle_receive(const boost::system::error_code& ec,
+		std::size_t /*bytes_transferred*/)
+	{
+		if (ec) {
+			std::cout << "SERVER: " << ec.message() << std::endl;
+			return;
+		}
 
-    while (it != ba::ip::udp::resolver::iterator())
-    {
-        local_addr = (it++)->endpoint().address();
-        if (local_addr.is_v4()) {
-            break;
-        }
-    }*/
+		// async proxy creation and data transmission
+		proxies.push(std::make_shared<Proxy>(source_endpoint));
 
-    boost::system::error_code error;
-    std::cout << sizeof(error) << std::endl;
+		start_receive();
+	}
 
-    auto socket = std::make_shared< ba::ip::udp::socket>(io_context);
-    std::cout << sizeof(socket) << std::endl;
-    socket->open(ba::ip::udp::v4(), error);
-    if (error) {
-        std::cerr << "Socket open failed: " << error.message() << std::endl;
-    }
+	void handle_send(boost::shared_ptr<std::string> /*message*/,
+		const boost::system::error_code& /*error*/,
+		std::size_t /*bytes_transferred*/)
+	{
+	}
 
-    ba::socket_base::broadcast option(true);
-    socket->set_option(option);
+	// Implement proxy deletion once it finishes serving the client(change structure)
+	std::queue<std::shared_ptr<Proxy>> proxies;
+	ba::ip::udp::socket socket_;
+	ba::ip::udp::endpoint source_endpoint;
+	char recv_buffer_[MAX_LENGTH];
+};
 
-    ba::ip::udp::endpoint remote_endpoint = ba::ip::udp::endpoint(ba::ip::address_v4::broadcast(), BROADCAST_PORT);
-    std::cout << sizeof(remote_endpoint) << std::endl;
+int main(int argc, char* argv[]) {
+	boost::system::error_code ec;
 
-    const char* message = "I am still here";
-    int len = strlen(message);
-    ba::const_buffer buffer = ba::buffer(message, len + 1);
+	// should move these to pb class
+	auto socket = std::make_shared< ba::ip::udp::socket>(io_context);
+	socket->open(ba::ip::udp::v4(), ec);
+	if (ec) {
+		std::cerr << "Socket open failed: " << ec.message() << std::endl;
+	}
 
-    std::cout << sizeof(buffer) << std::endl;
+	ba::socket_base::broadcast option(true);
+	socket->set_option(option);
 
-    PeriodicBroadcasting pb(1);
+	ba::ip::udp::endpoint remote_endpoint = ba::ip::udp::endpoint(ba::ip::address_v4::broadcast(), BROADCAST_PORT);
 
-    pb.start(socket, buffer, remote_endpoint);
+	const char* message = "I am still here";
+	int len = strlen(message);
+	ba::const_buffer buffer = ba::buffer(message, len + 1);
 
-    
+	PeriodicBroadcasting pb(1);
 
-    ba::ip::tcp::resolver resolver(io_context);
-    auto endpoints = resolver.resolve("playground.tesonet.lt", "https");
+	pb.start(socket, buffer, remote_endpoint);
+	udp_server server;
+	std::cout << "Server is running." << std::endl;
 
-    boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
-    //ctx.load_verify_file("ca.pem");
-
-    client c(io_context, ctx, endpoints);
-
-    io_context.run();
-
-    return 0;
+	io_context.run();
+	std::this_thread::sleep_for(std::chrono::seconds(200));
+	return 0;
 }
